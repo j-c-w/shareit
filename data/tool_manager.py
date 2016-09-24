7f857584731a854ab3a5b4c999e6f11a32bd0f9b
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-24 18:34:15 +0100

    Fixed issues with getting JSON encoding done appropriately

--- data/tool_manager.py
+++ data/tool_manager.py
@@ -1,3 +1,5 @@
+import json
+
 class RentalTool(object):
     def __init__(self, name, descr, price, rented, pickupDate, returnDate):
         self.name = name
@@ -7,7 +9,12 @@ class RentalTool(object):
         self.pickupDate = pickupDate
         self.returnDate = returnDate
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o:
+                          o.__dict__, sort_keys=True,
+                          indent=4)
+
 def loadTools(configFile):
     # This loads in the various tools from the config file
-    return [RentalTool("Wrench", "A wrench", 100, False, None, None)]
+    return [RentalTool("Wrench", "A wrench", 100, False, None, None).toJSON()]
 
