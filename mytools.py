116a355161d0ec41447cdc26071e1e6520efa596
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-24 18:33:53 +0100

    Added some web frontend to the rest

--- mytools.py
+++ mytools.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_restful import Resource, Api
+from requests import put, get
 
 import payments.payment_manager
 import data.tool_manager
@@ -31,15 +32,35 @@ class RESTTool(Resource):
         tool.setMeetingTime = meetingTime
         tool.setCollectionTime = collectionTime
 
-        # Deal with the payment
-        payments.payment_manager.make_payment(producer, request.form['consumer'], request.form['amount'])
+        # Now, send confirmation email to the user
+        # with the QR code attached
+
 
         # Then return a confirmation
         return {"tool reserved": True}
 
 
-
 api.add_resource(RESTTool, '/myTools')
+
+
+@app.route("/")
+def localLibraries():
+    # TODO -- use the central server to lookup
+    # Other IP addresses. For now, use local
+
+    # For demo, setup an IP
+    return "<a href='/show/localhost'>172.16.14.105</a>"
+
+
+@app.route("/show/<ip>")
+def display(ip):
+    address = "http://" + ip + ":5000/myTools"
+    print address
+
+    data = get(address)
+
+    print data
+    return "Yay"
 
 if __name__ == '__main__':
     # Load the tools up from the config file
