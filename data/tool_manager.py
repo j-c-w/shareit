9da659096ac0842b547189375a130e6fc0539e57
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-24 23:01:46 +0100

    Added a new ID field

--- data/tool_manager.py
+++ data/tool_manager.py
@@ -1,7 +1,8 @@
 import json
 
 class RentalTool(object):
-    def __init__(self, name, descr, price, rented, pickupDate, returnDate):
+    def __init__(self, id, name, descr, price, rented, pickupDate, returnDate):
+        self.id = id
         self.name = name
         self.descr = descr
         self.price = price
@@ -16,5 +17,5 @@ class RentalTool(object):
 
 def loadTools(configFile):
     # This loads in the various tools from the config file
-    return [RentalTool("Wrench", "A wrench", 100, False, None, None).toJSON()]
+    return [RentalTool("WRENCH", "Wrench", "A wrench", 100, False, None, None).toJSON()]
 
