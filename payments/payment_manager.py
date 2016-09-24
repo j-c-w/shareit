fe342d91ef61a82309cdab3c0e8c8ae73674e990
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-24 14:37:52 +0100

    Added skeleton for worldpay payments

--- payments/payment_manager.py
+++ payments/payment_manager.py
@@ -1,2 +1,9 @@
 # The idea here is to interact with the
 # payments SDK.
+
+"""
+This sends a payment of value 'amount' from
+'username_from' to 'username_to'
+"""
+def make_payment(username_from, username_to, amount):
+    pass
