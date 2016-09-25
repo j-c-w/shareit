c26fbc894009a7b1bf45edd45d987ef40884882b
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-25 13:17:21 +0100

    Fixed bug with JSON not being loaded

--- data/tool_manager.py
+++ data/tool_manager.py
@@ -47,6 +47,8 @@ def loadTools(configFile=".tools"):
 
 def item_from_id(item_id):
     for item in tools:
+        # That is going to be slow
+        item = json.load(item)
         if item['id'] == item_id:
             return item
 
