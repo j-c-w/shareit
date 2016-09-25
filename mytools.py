e7e678e595cc0407bf3ccb84d1aea2814d1f6911
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-25 10:10:47 +0100

    Fixed bug where response was not converted to JSON

--- mytools.py
+++ mytools.py
@@ -62,7 +62,7 @@ api.add_resource(RESTTool, '/myTools')
 def localLibraries():
     # Lookup the list of IP addresses from the name server:
     nameserver_address = "http://" + nameserver_ip + "/getLibraries"
-    ips = put(nameserver_address, {'address': postal_address, 'ip': nameserver_ip})['ips']
+    ips = put(nameserver_address, {'address': postal_address, 'ip': nameserver_ip}).json()['ips']
     print ips
 
     return render_template("html/ip_list.html", ips=ips)
