9f7d90d7d2aa199e60b4d92b07ff36013b79c59e
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-25 02:14:26 +0100

    Added the nameserver code

---
+++ data/library_manager.py
@@ -0,0 +1,60 @@
+import json
+import threading #aaaahhhh
+import time
+
+"""
+The idea of this class is that
+it is used to manage the libraries.
+
+For the moment it writes out to disk and then
+copies it all back in.
+
+Ideally will use MongoDB in the future to
+deal with the writing and reading
+"""
+
+# In seconds
+TTL = 30
+
+class LibraryManager(object):
+    def __init__(self, datafile=".libraryData"):
+        self.data = []
+        # Also need to start the expiry timer:
+        threading.Timer(30.0, self._cleanup).start()
+
+    """ Takes a dictionary with 'ip', 'address' and
+    'id' defined"""
+    def update(self, message):
+        message_id = message['id']
+        for item, index in enumerate(self.data):
+            if item['id'] == message_id:
+                # Then replace the item here
+                message['refreshTime'] = time.time()
+                self.data[index] = message
+
+    """ This goes through the data list and
+    clears out all the data with timestamps
+    that were too long ago"""
+    def _cleanup(self):
+        current_time = time.time()
+
+        for item, index in enumerate(self.data):
+            if current_time - item['refreshTime'] > TTL:
+                # delete the item and remove it:
+                del self.data[index]
+
+        # Then reschedule itself
+        threading.Timer(30.0, self._cleanup).start()
+
+    def getIPsNear(self, address):
+        # todo -- implement a search for nearby addresses
+
+        ips = []
+
+        for item in self.data:
+            ips.append(item['ip'])
+
+        return ips
+
+
+
---
+++ nameserver.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template
+from flask_restful import Resource, Api
+from requests import put, get
+
+import uuid
+import data.library_manager
+
+"""
+This is the server that deals with dishing out
+the IPs and home addresses of the different
+servers.
+
+When a server starts up, it aims to provide
+its information to the server so that the server
+can dish that out to others that request
+that information.
+
+We make a decision based on locality as to which
+servers we respond with first
+"""
+
+app = Flask(__name__, template_folder="static")
+api = Api(app)
+
+ips = data.library_manager.LibraryManager()
+
+
+"""
+This displays IPs in a user readable form
+"""
+
+
+@app.route("/")
+def all_ips():
+    ip_list = ips.getIPsNear("")
+    # return " Hi there"
+    # print ip_list
+    return str(ip_list)
+
+
+class ListFetch(Resource):
+    def put(self):
+        # Returns a JSON list of all the IPs
+        # todo -- take the address of the
+        # sender and calculate the ones within
+        # range
+
+        address = request.form['address']
+
+        # return just the ip addresses for
+        # the sake of security
+        return {'ips': ips.getIPsNear(address)}
+
+
+class ListSet(Resource):
+    def put(self):
+        ip = request.form['data']
+        address = request.form['address']
+
+        # The ID is a unique way for this
+        #
+        if request.form['id']:
+            id = request.form['id']
+        else:
+            id = generate_id()
+
+        # Now, create an entry in the list
+        ips.update({'ip': ip, 'address': address,
+                    'id': id})
+
+        # Return the id as confirmation
+        return {'id': id}
+
+
+api.add_resource(ListFetch, "/getLibraries")
+api.add_resource(ListSet, "/ping")
+
+
+def generate_id():
+    return uuid.uuid4()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
