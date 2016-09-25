b2053d8bfa57a76a0299b9b565677fb8b4007611
Jackson Woodruff <woodruff.jackson@gmail.com>
2016-09-25 14:31:36 +0100

    more email fxing

--- mytools.py
+++ mytools.py
@@ -195,7 +195,7 @@ def rent(ip, id):
     with smtplib.SMTP(host='smtp.gmail.com',port=587) as server:
         server.starttls()
         server.login('sharelet134', 'sharelet9999')
-        server.sendmail(fromAddress, email, toAddr)
+        server.sendmail(fromAddress, toAddr, email)
 
     return "Confirmation emails sent! Go to " + \
             destination_postal_address + """ to
@@ -263,7 +263,7 @@ class RentRequest(Resource):
         server = smtplib.SMTP(host='smtp.gmail.com', port=587)
         server.starttls()
         server.login('sharelet134', 'sharelet9999')
-        server.sendmail(fromAddress, useremail, toAddr)
+        server.sendmail(fromAddress, toAddr, email)
         server.quit()
 
         print "email sent"
